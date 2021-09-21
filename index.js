7f6f15e0c24ed58c91460c4014df26f6026ad143
Samantha Ostrowski <isphinxs@hotmail.com>
2021-09-20 20:47:38 -0400

    commit tree e1abafaceafacc8bb9b39030e60c11f3fb3fae36

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 function largestSubarraySum(arr) {
-    let start = 0;
-    let end = 0;
-    let max = arr[0];
+    // let start = 0;
+    // let end = 0;
+    // let max = arr[0];
 
-    for (let i = 0; i < arr.length; i++) {
-        let sum = arr[i];
-        for (let j = i + 1; j < arr.length; j++) {
-            sum += arr[j];
-            if (sum > max) {
-                start = i;
-                end = j;
-                max = sum;
-            }
+    // for (let i = 0; i < arr.length; i++) {
+    //     let sum = arr[i];
+    //     for (let j = i + 1; j < arr.length; j++) {
+    //         sum += arr[j];
+    //         if (sum > max) {
+    //             start = i;
+    //             end = j;
+    //             max = sum;
+    //         }
+    //     }
+    // }
+    
+    // if (max < 0) {
+    //     max = 0;
+    // }
+    
+    // return max;
+
+    // Kandane's Algorithm
+
+    let localMax = arr[0];
+    let globalMax = 0;
+
+    for (let i = 1; i < arr.length; i++) {
+        localMax = Math.max(arr[i], localMax + arr[i]);
+        if (localMax > globalMax) {
+            globalMax = localMax;
         }
     }
-    
-    if (max < 0) {
-        max = 0;
-    }
-    
-    return max;
+    return globalMax;
+
 }
